6fde2dfcf1c13595371c7e9ff2293d773b544af1
Akihiro Suzuki <28677705+akhrszk@users.noreply.github.com>
2021-12-19 22:17:57 +0900

    :hammer: Add husky

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import { MemoryRouter } from 'react-router'
 
 test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  )
+  const titleElement = screen.getByText(/clock/i)
+  expect(titleElement).toBeInTheDocument()
 })
